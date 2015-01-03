3bad57aff03525581ca5f3eb3181eb7f62dfac7d
schnie <greg@differential.com>
2015-01-03 14:02:36 -0500

    version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'differential:workers',
   summary: 'Spawn headless worker meteor processes to work on async jobs',
-  version: '1.0.0',
+  version: '1.1.0',
   git: 'https://github.com/Differential/meteor-workers'
 });
 
